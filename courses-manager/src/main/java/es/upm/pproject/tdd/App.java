f1c07acb48466c76a6cd653a540317cc8270f95e
victor nieves sanchez <vnievess@gmail.com>
2018-09-26 08:53:24 +0200

    Updated hello to print Hello if there is 1 input argument. O.C. prints that cant say hello.

--- courses-manager/src/main/java/es/upm/pproject/tdd/App.java
+++ courses-manager/src/main/java/es/upm/pproject/tdd/App.java
@@ -1,4 +1,5 @@
 package es.upm.pproject.tdd;
+import java.io.*;
 
 /**
  * Hello world!
@@ -8,6 +9,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	if (args.length == 1)
+	        System.out.println( "Hello World!" );
+	else if (args.length == 0)
+		System.out.println("No arguments, no hello.");	
+	else
+		System.out.println("Too many arguments!");
     }
 }
