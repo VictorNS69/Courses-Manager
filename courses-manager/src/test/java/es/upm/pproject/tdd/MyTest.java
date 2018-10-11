402ccafbfe7ff37c08684de1f7c662809ffb30f2
Victor Nieves <vnievess@gmail.com>
2018-10-11 19:10:22 +0200

    Added new body of the program

---
+++ courses-manager/src/test/java/es/upm/pproject/tdd/MyTest.java
@@ -0,0 +1,13 @@
+package src.java.es.upm.pproject.tdd;
+
+import java.io.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyTest{
+  @Test
+  void Apptest (){
+    int one=1;
+    assertEquals(one,1);
+  }
+}
