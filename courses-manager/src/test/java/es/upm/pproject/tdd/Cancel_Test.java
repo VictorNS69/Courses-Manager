bea08d19b75432cfa149565c6cd636df5e3b1164
Victor Nieves Sanchez <victor.nieves.sanchez@alumnos.upm.es>
2018-10-22 22:30:13 +0200

    added new test class Cancel_Test

---
+++ courses-manager/src/test/java/es/upm/pproject/tdd/Cancel_Test.java
@@ -0,0 +1,105 @@
+package es.upm.pproject.tdd;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+
+public class Cancel_Test {
+	private ArrayList <Student> students;
+	private ArrayList <Course>courses;
+	private Manager manager;
+	
+	@BeforeEach
+	private void init() throws MyException {
+		this.students = new ArrayList <Student>();
+		this.courses = new ArrayList <Course>();
+		this.manager = new Manager(students, courses);
+	}
+	
+	@Test
+	public void cancel_empty_students_list_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			//this.manager.new_student(1, "Victor", "email@test.com");
+			//Student s = new Student(1, "Victor", "email@test.com");
+			this.manager.new_course(1, "Programming Project", "Guillermo");
+			this.manager.cancel(this.students.get(0), this.courses.get(0));
+			});
+	}
+	
+
+	@Test
+	public void cancel_empty_courses_list_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			this.manager.new_student(1, "Victor", "email@test.com");
+			//this.manager.new_course(1, "Programming Project", "Guillermo");
+			this.manager.cancel(this.students.get(0), this.courses.get(0));
+			});
+	}
+	
+
+	@Test
+	public void cancel_null_student_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			this.manager.new_course(1, "Programming Project", "Guillermo");
+			this.manager.cancel(null, this.courses.get(0));
+			});
+	}
+	
+	@Test
+	public void cancel_null_course_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			this.manager.new_student(1, "Victor", "email@test.com");
+			//Student s = new Student(1, "Victor", "email@test.com");
+			this.manager.cancel(this.students.get(0), null);
+			});
+	}
+	
+	@Test
+	public void cancel_ok_1_test() throws MyException {
+		this.manager.new_student(1, "Victor", "email@test.com");
+		this.manager.new_course(1, "Programming Project", "Guillermo");
+		this.manager.enroll_student(1, 1);
+		this.manager.cancel(this.students.get(0),this.courses.get(0));
+		assertEquals(1, this.students.size());
+		assertEquals(0,this.courses.get(0).getStudents().size());
+	}
+	
+	@Test
+	public void cancel_ok_2_test() throws MyException {
+		this.manager.new_student(1, "Victor", "email@test.com");
+		this.manager.new_student(2, "Alejandro", "email@test.com");
+		this.manager.new_course(1, "Programming Project", "Guillermo");
+		this.manager.enroll_student(1, 1);
+		this.manager.enroll_student(2, 1);
+		this.manager.cancel(this.students.get(0),this.courses.get(0));
+		assertEquals(2, this.students.size());
+		assertEquals(1,this.courses.get(0).getStudents().size());
+	}
+	
+	@Test
+	public void cancel_student_not_in_course_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			//this.manager.new_student(1, "Victor", "email@test.com");
+			this.manager.new_course(1, "Programming Project", "Guillermo");
+			this.manager.cancel(this.students.get(0), this.courses.get(0));
+			});
+	}
+	
+	@Test
+	public void cancel_course_not_in_system_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			this.manager.new_student(1, "Victor", "email@test.com");
+			this.manager.new_course(1, "Programming Project", "Guillermo");
+			this.manager.cancel(this.students.get(0), this.courses.get(2));
+			});
+	}
+	
+	@Test
+	public void cancel_student_not_in_system_test() throws MyException {
+		assertThrows(Exception.class, ()->{
+			this.manager.new_student(1, "Victor", "email@test.com");
+			this.manager.new_course(1, "Programming Project", "Guillermo");
+			this.manager.cancel(this.students.get(2), this.courses.get(1));
+			});
+	}
+}
